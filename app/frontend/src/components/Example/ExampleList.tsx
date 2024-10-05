2376709091c7b1c6cf08597758b49a28454a3771
Zvonimir Mavretić <zvonimir.mavretic@asseco-see.hr>
2024-10-05 19:57:23 +0100

    Prijevod na hrvatski

--- app/frontend/src/components/Example/ExampleList.tsx
+++ app/frontend/src/components/Example/ExampleList.tsx
@@ -11,9 +11,7 @@ export type ExampleModel = {
 };
 
 const EXAMPLES: ExampleModel[] = [
-    { text: "Are there any radio telescopes in Australia?", value: "Are there any radio telescopes in Australia?" },
-    { text: "What are Microsoft's primary sources of revenue?", value: "What are Microsoft's primary sources of revenue?" },
-    { text: "What are some flavors of Breyers?", value: "What are some flavors of Breyers?" }
+    { text: "Odluke Vrhovnog suda u 2024. godini vezane uz radno pravo?", value: "Odluke Vrhovnog suda u 2024. godini vezane uz radno pravo?" }
 ];
 
 interface Props {
