648d66b6ebc55f797e46a307e3b406a0762127e0
ZrinkaGligo <zrinka.gligo@asseco-see.hr>
2024-11-26 07:59:06 +0000

    Added Legal assistant UX

--- app/frontend/src/components/LegalAssistant/LegalAssistant.tsx
+++ app/frontend/src/components/LegalAssistant/LegalAssistant.tsx
@@ -1,11 +1,46 @@
-function LegalAssistant(){
+import {DropZone} from '../filepicker/drop-zone';
+import { nanoid } from "nanoid";
+import React, {useState, createContext} from "react";
+import styles from './LegalAssistant.module.css';
+
+
+interface Props {
+    folderPath: string;
+    tags: string[];
+  }
+
+    const LegalAssistant = ({folderPath, tags}: Props) => {
+    const [files, setFiles] = useState<any>([]);
+        
+    const handleOnChange = () => {}
     return (
         <>
-            <div>
-            <h1>Legal Assistant</h1>
+            <div className={styles.mainDiv}>
+                {/* <h2>Hello from Legal Assistant</h2> */}
+                <div className={styles.dropZoneStyle}>
+                    <DropZone onChange={handleOnChange} accept={files}/>
+                </div>      
+                <div className={styles.actionsStyles}>
+                     <span className={styles.btnStyle}>
+                        <p className={styles.exampleText}>
+                            Generiranje sažetka dokumenata
+                        </p>
+                    </span>
+                    <span className={styles.btnStyle}>
+                        <p className={styles.exampleText}>
+                            Dodavanje dokumenata <br/>u blob storage
+                        </p>
+                    </span>
+                    <span className={styles.btnStyle}>
+                        <p className={styles.exampleText}>
+                            Generiranje prijedloga odluke
+                        </p>
+                    </span>
+                </div> 
             </div>
+
         </>
     );
-}
+};
 
-export default LegalAssistant;
+export {LegalAssistant};
--- app/frontend/src/components/LegalAssistant/LegalAssistantEntry.tsx
+++ app/frontend/src/components/LegalAssistant/LegalAssistantEntry.tsx
@@ -1,16 +1,19 @@
 import styles from './LegalAssistantEntry.module.css';
 import legalAssistantIcon from '../../assets/LegalAssistant.png';
 
-function LegalAssistantEntry(){
+interface LegalAssistantEntryProps{
+    onLegalAssistantEntryClicked: (upit: string) => void;
+}
+export const LegalAssistantEntry = ({onLegalAssistantEntryClicked} : LegalAssistantEntryProps) => {
     return (
             <>
                 <div className={styles.wrap}> 
                     <img src={legalAssistantIcon} alt="Description of image" className={styles.image} />
                     <h1> Napredni pomoćnik</h1>
-                    <div className={styles.legalAssistantEntry} >
+                    <div className={styles.legalAssistantEntry} 
+                        onClick={()=>onLegalAssistantEntryClicked('TEST')}>
                         <p className={styles.exampleText}>
-                            Generiranje sažetka dokumenata
-                            <br/>
+                            Generiranje sažetka dokumenata<br/>
                             Generiranje prijedloga odluke
                         </p>
                     </div>
